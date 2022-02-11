3322ecc30c4f4abe6e89243e12ac43bb252bfe19
Romeu Gomes <romeugomes88@gmail.com>
2022-02-10 22:21:24 -0300

    Anotacao de @CrossOrigin("*")

--- src/main/java/com/romeu/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/romeu/bookstore/resources/CategoriaResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.romeu.bookstore.domain.Categoria;
 import com.romeu.bookstore.dtos.CategoriaDTO;
 import com.romeu.bookstore.service.CategoriaService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
--- src/main/java/com/romeu/bookstore/resources/LivroResource.java
+++ src/main/java/com/romeu/bookstore/resources/LivroResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.romeu.bookstore.domain.Livro;
 import com.romeu.bookstore.dtos.LivroDTO;
 import com.romeu.bookstore.service.LivroService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/livros")
 public class LivroResource {
